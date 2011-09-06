d9e40b5fd966b05e7b58f0f2893c63ff60fa7ff5
Christian Mayne <christian@christian-mayne.net>
2011-09-06 13:05:14 +0100

    Added layout links (with routes)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -22,10 +22,8 @@ describe PagesController do
     it "should have a non-blank body" do
       get 'home'
       response.body.should_not =~ /<body>\s*<\/body>/
-    end
-       
+    end       
   end
-
 
   describe "GET 'contact'" do
     it "should be successful" do
@@ -38,7 +36,6 @@ describe PagesController do
       response.should have_selector("title", 
                                     :content => "#{@base_title} | Contact")
     end    
-
   end
 
   describe "GET 'about'" do
@@ -52,7 +49,18 @@ describe PagesController do
       response.should have_selector("title", 
                                     :content => "#{@base_title} | About")
     end    
-
   end
 
+ describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", 
+                                    :content => "#{@base_title} | Help")
+    end    
+  end
 end
